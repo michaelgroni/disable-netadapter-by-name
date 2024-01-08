5646667ed4dfec00b84bd6d74b2b296f8a112826
michaelgroni <75781695+michaelgroni@users.noreply.github.com>
2024-01-08 15:16:00 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,12 +1,59 @@
-# disable network adapters if the network name contains $notWanted
+# Disable network adapters or IP protocols if the network name contains $notWanted.
 
-$notWanted = "mschool"
+# You can see the network name, for example, with the PowerShell command Get-NetConnectionProfile.
+
+## configuration
+
+$notWanted = "labor" # Not case-sensitive. It is sufficient to enter a part of the name.
+
+$disableCompletely = $false
+$disableIPv4 = $true
+$disableIPv6 = $true
+
+
+# When $disableCompletely is true, the network adapter ist disabled and the other variables are ignored.
+# Otherwise the network adapter stays enabled with the IP protocols switced on or off according to the variables.
+
+# WARNING: This script only detects an active network adapter. If you have completely disabled it or switched off
+#          both IP protocols, you have to reactivate ist manually.
+
+
+## script
+
 $notWanted = $notWanted.ToUpper()
 
 ForEach ($profile in Get-NetConnectionProfile)
-{
+{    
     if ($profile.Name.ToUpper().Contains($notWanted))
     {
-        Get-NetAdapter | Where InterfaceIndex -eq $profile.InterfaceIndex | Disable-NetAdapter -Confirm:$false
+        $adapters = Get-NetAdapter | Where InterfaceIndex -eq $profile.InterfaceIndex
+        $adapter = $adapters[0]
+        
+        if ($disableCompletely)
+        {
+            Disable-NetAdapter $adapter.Name -Confirm:$false
+        }
+        else # enable the network adapter
+        {
+            if ($disableIPv4)
+            {
+                Disable-NetAdapterBinding $adapter.Name -ComponentID ms_tcpip
+            }
+            else
+            {
+                Enable-NetAdapterBinding $adapter.Name -ComponentID ms_tcpip
+            }
+
+
+            if ($disableIPv6)
+            {
+                Disable-NetAdapterBinding $adapter.Name -ComponentID ms_tcpip6   
+            }
+            else
+            {
+                Enable-NetAdapterBinding $adapter.Name -ComponentID ms_tcpip6
+            }
+        }
+        
     }
 }
